19e9b568b1a68e9a2b87fdd4560cf552e7d3f4ef
Tim Jackson <timjacksonm@gmail.com>
2021-11-15 21:48:47 -0600

    added compression for responses before routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+var compression = require('compression');
 
 var app = express();
 
@@ -26,6 +27,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression()); //Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
